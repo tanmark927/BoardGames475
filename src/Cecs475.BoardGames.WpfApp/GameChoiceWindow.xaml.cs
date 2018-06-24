a29f844f05c200e54f3eafe6e895fe939ec31ec2
Mark Tan <damtan1996@gmail.com>
2018-06-24 14:27:08 -0700

    wpf app files

---
+++ src/Cecs475.BoardGames.WpfApp/GameChoiceWindow.xaml.cs
@@ -0,0 +1,84 @@
+﻿using Cecs475.BoardGames.WpfView;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Cecs475.BoardGames.WpfApp {
+    /// <summary>
+    /// Interaction logic for GameChoiceWindow.xaml
+    /// </summary>
+    public partial class GameChoiceWindow : Window {
+        public GameChoiceWindow() {
+            InitializeComponent();
+            Type IWpfGameFactory = typeof(IWpfGameFactory);
+
+            var files = Directory.GetFiles("games");
+            foreach (var f in Directory.GetFiles("games")) {
+                Console.WriteLine(f);
+
+                // Strip " games\ " folder name
+                string file = f.Substring(6);
+
+                // strips .dll ending
+                file = file.Substring(0, file.Length - 4);
+
+                // Load from assembly for strong named
+                Assembly.Load($"{file}, Version=1.0.0.0, Culture=neutral, PublicKeyToken=68e71c13048d452a");
+
+                //// if file ends with dll
+                // if (file.Substring(file.Length - 3) == "dll") {
+                
+                // Load file
+                //Assembly.LoadFrom(f);
+
+                
+            }
+
+            List<object> GamesList = new List<object>();
+            var boardTypes = AppDomain.CurrentDomain.GetAssemblies()
+                .SelectMany(a => a.GetTypes())
+                .Where(t => IWpfGameFactory.IsAssignableFrom(t) && t.IsClass);
+            foreach (var val in boardTypes) {
+                
+                IWpfGameFactory v = (IWpfGameFactory)val.GetConstructor(Type.EmptyTypes).Invoke(null);
+                GamesList.Add(v);
+            }
+            this.Resources["GameTypes"] = GamesList;
+        }
+
+
+        private void Button_Click(object sender, RoutedEventArgs e) {
+            Button b = sender as Button;
+            // Retrieve the game type bound to the button
+            IWpfGameFactory gameType = b.DataContext as IWpfGameFactory;
+            // Construct a GameWindow to play the game.
+            var gameWindow = new GameWindow(gameType,
+                mHumanBtn.IsChecked.Value ? NumberOfPlayers.Two : NumberOfPlayers.One)
+            {
+                Title = gameType.GameName
+            };
+            // When the GameWindow closes, we want to show this window again.
+            gameWindow.Closed += GameWindow_Closed;
+
+            // Show the GameWindow, hide the Choice window.
+            gameWindow.Show();
+            this.Hide();
+        }
+
+        private void GameWindow_Closed(object sender, EventArgs e) {
+            this.Show();
+        }
+    }
+}
---
+++ src/Cecs475.BoardGames.WpfApp/LoadGames.xaml.cs
@@ -0,0 +1,66 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Cecs475.BoardGames.WpfApp
+{
+    /// <summary>
+    /// Interaction logic for LoadGames.xaml
+    /// </summary>
+    public partial class LoadGames : Window
+    {
+        public LoadGames()
+        {
+            InitializeComponent();
+        }
+
+        private async void mLoaded(object sender, RoutedEventArgs e)
+        {
+            var client = new RestClient("https://cecs475-boardamges.herokuapp.com/");
+            var request = new RestRequest("api/games", Method.GET);
+            var response = await client.ExecuteTaskAsync(request);
+
+            string r = response.Content;
+            r = r.Substring(1);
+            r = r.Substring(0, r.Length - 1);
+            var games = JObject.Parse(r);
+            JToken results = games.Last.First;
+            List<string> strList = new List<string>();
+            List<JToken> things = new List<JToken>() { results.First, results.Last };
+            foreach (JToken t in things) {
+                JToken token = t.First;
+                do {
+                    strList.Add(token.First.ToObject<string>());
+                    token = token.Next;
+                }
+                while (token != null);
+            }
+            for (int i = 0; i < strList.Count(); i++) {
+                string val = strList.ElementAt(i);
+                if (val.Contains("https")) {
+                    var web = new WebClient();
+                    await web.DownloadFileTaskAsync(val, "games/" + strList.ElementAt(i - 1));
+                }
+            }
+            
+            
+            GameChoiceWindow cgw = new GameChoiceWindow();
+            cgw.Show();
+            this.Close();
+        }
+    }
+}
