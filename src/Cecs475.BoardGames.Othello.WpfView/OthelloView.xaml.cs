0d87120b2beebae23f1495eb1dcf7342028a7d8e
Mark Tan <damtan1996@gmail.com>
2018-06-24 14:21:18 -0700

    othello wpf view files

---
+++ src/Cecs475.BoardGames.Othello.WpfView/OthelloView.xaml.cs
@@ -0,0 +1,65 @@
+﻿using Cecs475.BoardGames.WpfView;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Cecs475.BoardGames.Othello.WpfView {
+	/// <summary>
+	/// Interaction logic for OthelloView.xaml
+	/// </summary>
+	public partial class OthelloView : UserControl, IWpfGameView {
+		public OthelloView() {
+			InitializeComponent();
+		}
+
+		private void Border_MouseEnter(object sender, MouseEventArgs e) {
+            if (!IsEnabled) return;
+            Border b = sender as Border;
+			var square = b.DataContext as OthelloSquare;
+			var vm = FindResource("vm") as OthelloViewModel;
+			if (vm.PossibleMoves.Contains(square.Position)) {
+				square.IsHighlighted = true;
+			}
+		}
+
+		private void Border_MouseLeave(object sender, MouseEventArgs e) {
+            if (!IsEnabled) return;
+            Border b = sender as Border;
+			var square = b.DataContext as OthelloSquare;
+			square.IsHighlighted = false;
+		}
+
+		public OthelloViewModel OthelloViewModel => FindResource("vm") as OthelloViewModel; 
+
+		public Control ViewControl => this;
+
+		public IGameViewModel ViewModel => OthelloViewModel;
+
+		private async void Border_MouseUp(object sender, MouseButtonEventArgs e) {
+            if (!IsEnabled) return;
+            Border b = sender as Border;
+			var square = b.DataContext as OthelloSquare;
+			var vm = FindResource("vm") as OthelloViewModel;
+			if (vm.PossibleMoves.Contains(square.Position)) {
+                IsEnabled = false;
+                vm.UndoEnabler = false;
+                await vm.ApplyMove(square.Position);
+                IsEnabled = true;
+                vm.UndoEnabler = true;
+				square.IsHighlighted = false;
+			}
+		}
+	}
+}
---
+++ src/Cecs475.BoardGames.Othello.WpfView/OthelloViewModel.cs
@@ -0,0 +1,194 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.ComponentModel;
+using System.Collections.ObjectModel;
+using Cecs475.BoardGames.WpfView;
+using System;
+
+using Cecs475.BoardGames.Othello.Model;
+using Cecs475.BoardGames.Model;
+using Cecs475.BoardGames.ComputerOpponent;
+using System.Threading.Tasks;
+
+namespace Cecs475.BoardGames.Othello.WpfView {
+	/// <summary>
+	/// Represents one square on the Othello board grid.
+	/// </summary>
+	public class OthelloSquare : INotifyPropertyChanged {
+		private int mPlayer;
+		/// <summary>
+		/// The player that has a piece in the given square, or 0 if empty.
+		/// </summary>
+		public int Player {
+			get { return mPlayer; }
+			set {
+				if (value != mPlayer) {
+					mPlayer = value;
+					OnPropertyChanged(nameof(Player));
+				}
+			}
+		}
+
+		/// <summary>
+		/// The position of the square.
+		/// </summary>
+		public BoardPosition Position {
+			get; set;
+		}
+
+
+		private bool mIsHighlighted;
+		/// <summary>
+		/// Whether the square should be highlighted because of a user action.
+		/// </summary>
+		public bool IsHighlighted {
+			get { return mIsHighlighted; }
+			set {
+				if (value != mIsHighlighted) {
+					mIsHighlighted = value;
+					OnPropertyChanged(nameof(IsHighlighted));
+				}
+			}
+		}
+
+		public event PropertyChangedEventHandler PropertyChanged;
+		private void OnPropertyChanged(string name) {
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+		}
+	}
+
+	/// <summary>
+	/// Represents the game state of a single othello game.
+	/// </summary>
+	public class OthelloViewModel : INotifyPropertyChanged, IGameViewModel {
+		private OthelloBoard mBoard;
+		private ObservableCollection<OthelloSquare> mSquares;
+		public event EventHandler GameFinished;
+		private const int MAX_AI_DEPTH = 6;
+		private IGameAi mGameAi = new MinimaxAi(MAX_AI_DEPTH);
+
+        private bool enabler;
+        public bool UndoEnabler
+        {
+            get { return enabler; }
+            set
+            {
+                enabler = value;
+                OnPropertyChanged(nameof(CanUndo));
+            }
+        }
+
+        public OthelloViewModel() {
+			mBoard = new OthelloBoard();
+
+			// Initialize the squares objects based on the board's initial state.
+			mSquares = new ObservableCollection<OthelloSquare>(
+				BoardPosition.GetRectangularPositions(8, 8)
+				.Select(pos => new OthelloSquare() {
+					Position = pos,
+					Player = mBoard.GetPlayerAtPosition(pos)
+				})
+			);
+
+			PossibleMoves = new HashSet<BoardPosition>(
+				from OthelloMove m in mBoard.GetPossibleMoves()
+				select m.Position
+			);
+		}
+
+		/// <summary>
+		/// Applies a move for the current player at the given position.
+		/// </summary>
+		public async Task ApplyMove(BoardPosition position) {
+			var possMoves = mBoard.GetPossibleMoves() as IEnumerable<OthelloMove>;
+			// Validate the move as possible.
+			foreach (var move in possMoves) {
+				if (move.Position.Equals(position)) {
+					mBoard.ApplyMove(move);
+					break;
+				}
+			}
+
+			if (Players == NumberOfPlayers.One && !mBoard.IsFinished) {
+                var bestMove = await Task.Run(() => mGameAi.FindBestMove(mBoard));
+                if (bestMove != null) {
+					mBoard.ApplyMove(bestMove as OthelloMove);
+				}
+			}
+
+			RebindState();
+
+			if (mBoard.IsFinished) {
+				GameFinished?.Invoke(this, new EventArgs());
+			}
+		}
+
+		private void RebindState() {
+			// Rebind the possible moves, now that the board has changed.
+			PossibleMoves = new HashSet<BoardPosition>(
+				from OthelloMove m in mBoard.GetPossibleMoves()
+				select m.Position
+			);
+
+			// Update the collection of squares by examining the new board state.
+			var newSquares = BoardPosition.GetRectangularPositions(8, 8);
+			int i = 0;
+			foreach (var pos in newSquares) {
+				mSquares[i].Player = mBoard.GetPlayerAtPosition(pos);
+				i++;
+			}
+			OnPropertyChanged(nameof(BoardAdvantage));
+			OnPropertyChanged(nameof(CurrentPlayer));
+			OnPropertyChanged(nameof(CanUndo));
+		}
+
+		/// <summary>
+		/// A collection of 64 OthelloSquare objects representing the state of the 
+		/// game board.
+		/// </summary>
+		public ObservableCollection<OthelloSquare> Squares {
+			get { return mSquares; }
+		}
+
+		/// <summary>
+		/// A set of board positions where the current player can move.
+		/// </summary>
+		public HashSet<BoardPosition> PossibleMoves {
+			get; private set;
+		}
+
+		/// <summary>
+		/// The player whose turn it currently is.
+		/// </summary>
+		public int CurrentPlayer {
+			get { return mBoard.CurrentPlayer; }
+		}
+
+		/// <summary>
+		/// The value of the othello board.
+		/// </summary>
+
+		public GameAdvantage BoardAdvantage => mBoard.CurrentAdvantage;
+
+        public bool CanUndo => mBoard.MoveHistory.Any() && UndoEnabler;
+
+        public NumberOfPlayers Players { get; set; }
+
+		public event PropertyChangedEventHandler PropertyChanged;
+		private void OnPropertyChanged(string name) {
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+		}
+
+		public void UndoMove() {
+			if (CanUndo) {
+				mBoard.UndoLastMove();
+				// In one-player mode, Undo has to remove an additional move to return to the
+				// human player's turn.
+				if (Players == NumberOfPlayers.One && CanUndo) {
+					mBoard.UndoLastMove();
+				}
+				RebindState();
+			}
+		}
+	}
+}
